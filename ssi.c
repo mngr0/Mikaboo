4085e6652b1a58a2465471668aef5a910c23e1d0
Marco Negrini <marco.negrini6@studio.unibo.it>
2017-08-03 13:02:51 +0200

    another useless commit, to share code

--- ssi.c
+++ ssi.c
@@ -28,7 +28,9 @@
 unsigned int SSIdoRequest(unsigned int * msg_ssi, struct tcb_t* sender ,uintptr_t reply) {
 	unsigned int service;
 	service=*msg_ssi;
-
+	char t= 'a';
+	char *s=&t;
+	memaddr * base;
    // unsigned int payload = msg_ssi->payload;
    // sender = msg_ssi->sender;
 
@@ -50,9 +52,9 @@ unsigned int SSIdoRequest(unsigned int * msg_ssi, struct tcb_t* sender ,uintptr_
 		break;
 		case TERMINATE_THREAD :
 		break;
-		case SETPGMMGR :
+		case SETPGMMGR:
 		break;
-		case SETTLBMGR :
+		case SETTLBMGR:
 		break;
 		case SETSYSMGR:
 		break;
@@ -60,11 +62,12 @@ unsigned int SSIdoRequest(unsigned int * msg_ssi, struct tcb_t* sender ,uintptr_
 		break;
 		case WAIT_FOR_CLOCK:
 		break;
-		case DO_IO :
-		//devregtr * base = (devregtr *) (TERM0ADDR);
-		//*(base + 3) = PRINTCHR | (((devregtr) *s) << BYTELEN);
+		case DO_IO:
+		base = (memaddr *) (TERM0ADDR);
+		*(base + 3) = 2 | (((memaddr) *s) << 8);
+
 		//now put SSI (currenThread) in waiting Q , when interrupt will come 
-		// int handler will send ack and put ssi back in read Q
+		//int handler will send ack and put ssi back in read Q
 		break;
 		case GET_PROCESSID :
 		break;
