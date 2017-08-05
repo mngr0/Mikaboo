7d40b26d40beb84892375c4730a7538f3201d567
0ssigeno <simone.berni2@studio.unibo.it>
2017-08-05 15:54:35 +0200

    new scheduler

--- include/scheduler.h
+++ include/scheduler.h
@@ -5,8 +5,8 @@
 
 //#define BUS_INTERVALTIMER 0x10000020
 //#define BUS_TIMESCALE 0x10000024
-//#define SCHED_TIME_SLICE 100000
-//#define SCHED_PSEUDO_CLOCK 100000
+#define SCHED_TIME_SLICE 5000
+#define SCHED_PSEUDO_CLOCK 100000
 
 
 //#define SET_IT(timer_val) ((*((unsigned int *)BUS_INTERVALTIMER)) = (timer_val * (*(unsigned int *)BUS_TIMESCALE)))
@@ -22,9 +22,9 @@ struct tcb_t* currentThread;
 
 int threadCount;
 int softBlockCount;
-
-extern unsigned int slice_TOD;
-extern unsigned int process_TOD;
+//extern unsigned int clock_TOD;
+//extern unsigned int slice_TOD;
+//extern unsigned int process_TOD;
 
 int isTimer(unsigned int TIMER_TYPE);
 void scheduler();
--- scheduler.c
+++ scheduler.c
@@ -19,14 +19,57 @@ struct list_head terminal_queue[8] = {LIST_HEAD_INIT_ARRAY_8(terminal_queue)};
 */
 softBlockCount = 0; /* il numero di thread bloccai in attesa di I/O o di completare una richiesta dal SSI */
 
+unsigned int slice_TOD = 0;
+unsigned int clock_TOD = 0;
+unsigned int process_TOD=0;
 void prova(){}
 
 /**************************************************************/
 /* Funzione che deve gestire clock, pseudo-clock e scheduling */
 
 /**************************************************************/
-void scheduler() {
+int isTimer(unsigned int TIMER_TYPE){
+    int time_until_timer;
+    /* Calcola il tempo che manca allo scadere del timer di tipo TIMER_TYPE */
+    if(TIMER_TYPE == SCHED_TIME_SLICE){
+        time_until_timer= TIMER_TYPE - (getTODLO() - slice_TOD);
+    } else if(TIMER_TYPE == SCHED_PSEUDO_CLOCK){
+        time_until_timer= TIMER_TYPE - (getTODLO() - clock_TOD);
+    }
+    /* Se è scaduto ritorna true, altrimenti false */
+    if(time_until_timer <= 0){
+        return TRUE;
+    } else { 
+        return FALSE;
+    }
+}
+void setNextTimer(){
+    unsigned int TODLO = getTODLO();
+    /* Calcola il tempo trascorso dall'inizio del time slice corrente */
+    int time_until_slice = SCHED_TIME_SLICE - (TODLO - slice_TOD);
 
+    /* Se il time slice è appena teminato setta il prossimo */
+    if(time_until_slice<=0){
+        slice_TOD = TODLO;
+        time_until_slice= SCHED_TIME_SLICE;
+    }
+    
+    /* Calcola il tempo trascorso dall'inizio del ciclo di pseudo clock corrente */
+    int time_until_clock = SCHED_PSEUDO_CLOCK - (TODLO - clock_TOD);
+    /* Se il ciclo di pseudo clock è appena teminato setta il prossimo */
+    if(time_until_clock <= 0){
+        clock_TOD = TODLO;
+        time_until_clock = SCHED_PSEUDO_CLOCK;
+    }
+    /* Setta il prossimo timer */
+    if(time_until_slice <= time_until_clock) {
+        setTIMER(time_until_slice);
+    } else {
+        setTIMER(time_until_clock);
+    }
+}
+void scheduler() {
+	setNextTimer();
     /* azioni da compiere quando non c'e' nessun thread pronto ad eseguire */
     if (list_empty(&readyQueue) && currentThread == NULL) {
         prova();
@@ -43,8 +86,8 @@ void scheduler() {
               //  SET_IT(SCHED_PSEUDO_CLOCK);
             }
             /* impostiamo lo stato del processore con gli interrupt abilitati*/  //CHECK PLS
-            //setSTATUS(STATUS_ALL_INT_ENABLE(getSTATUS()));
-            for (;;);
+            setSTATUS(STATUS_ALL_INT_ENABLE(getSTATUS()));
+		WAIT();
         }
     }
  else {
@@ -73,6 +116,7 @@ void scheduler() {
            //     SET_IT(SCHED_TIME_SLICE);
 
 //        }
+	process_TOD=getTODLO();
         /* carico lo stato del thread nel processore  dalla sua tcb */
         LDST(&(currentThread->t_s));
     }
